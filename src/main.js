57cd450ace27cbf96bb38eb073325be12ccedd6d
yellow1Zero <18811465324@163.com>
2023-02-13 22:11:44 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,12 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载Vant核心组件库
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+// import { Button } from 'vant'
 
 // 加载全局样式
 import './style/index.less'
 import '@/style/index.css'
 
 Vue.config.productionTip = false
+// 注册使用Vant组件库
+Vue.use(Vant)
+// Vue.use(Button)
 
 new Vue({
   router,
